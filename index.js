5421b3d41898c65c6dd13aae1c09807c1a2d39e8
Victoria Daug <victoria.daug@gmail.com>
2018-05-04 17:19:58 +0100

    make it better

--- index.js
+++ index.js
@@ -31,12 +31,6 @@ app.use('/api', taskRouter);
 app.use('/api', workflowRouter);
 app.use('/api', commentRouter);
 
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   console.log(err)
