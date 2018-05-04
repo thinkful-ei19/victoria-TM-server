eb249e8ea032e4a5a04caba35c057a80080f14ee
Victoria Daug <victoria.daug@gmail.com>
2018-05-04 17:27:20 +0100

    make it better

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: 'http://localhost:3000' || process.env.CLIENT_ORIGIN,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
          'mongodb://localhost/taskme-backend' || process.env.DATABASE_URL,
   TEST_DATABASE_URL:
